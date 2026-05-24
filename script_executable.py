49f4e076e378973f9ab38d5a57d6c5cfaf258526
sarahamzallag <sarah.amzallag@etu.unistra.fr>
2026-05-24 15:31:51 +0200

    Reconstruction de la sequence

--- script_executable.py
+++ script_executable.py
@@ -1,44 +1,75 @@
 #!/usr/bin/env python3
 
 # script executable 
-#le rendre excutable : chmod +x script_excutable.py ds terminal sans ecrir python3 devant
-#on veut pouvoir faire ' script_excutable.py 8D28.pdb
+# le rendre executable : chmod +x script_executable.py ds terminal sans ecrire python3 devant
+# on veut pouvoir faire : ./script_executable.py 8D28.pdb
 
-
-
-
-import sys      #contient un list
+import sys      # contient une liste
 import RNA.utils as utils
               
 if len(sys.argv) != 2:
     print('Error')
     print('Usage:')
-    print ('>' +sys.argv[0] + 'file.pdb')
-    exit ()
+    print('>' + sys.argv[0] + ' file.pdb')
+    exit()
 
 pdb_name = sys.argv[1]
 
 
-with open (pdb_name, 'r') as file :
-    line = file.readline()  
-    while line [0:6].strip() != 'TER' : 
-        if line[0:7].strip() == 'ATOM': # Vérifie si la ligne correspond à un atome
+def extraire_donnees(pdb_name):
+    resultat = [] # Liste vide pour stocker les résultats 
 
-            name = line[12:16].strip()  # Nom de l'atome
-            
-            
-            if (name.startswith('N') or name.startswith('O'))and not name.startswith('OP') and "'" not in name: # On ne garde slmt les Azotes et  Oxygenes ET on eleve les ribose 
-             
-                base = line[17:20].strip() # Identification de la base (A,C,G,T)
-                chaine = line[21]            # Identification de la chaine (A, B)
-                position = int(line[22:26].strip()) # Position du nucléotide dans la chaîne
+    with open(pdb_name, 'r') as file:
+        line = file.readline()  
+        while line[0:6].strip() != 'TER': 
+            if line[0:7].strip() == 'ATOM': # Vérifie si la ligne correspond à un atome
+                name = line[12:16].strip()  # Nom de l'atome
+                
+                if (name.startswith('N') or name.startswith('O')) and not name.startswith('OP') and "'" not in name: 
+                    base = line[17:20].strip() # Identification de la base (A,C,G,U)
+                    chaine = line[21]            # Identification de la chaine (A, B)
+                    position = int(line[22:26].strip()) # Position du nucléotide
 
-                x = line[30:38].strip()     # Coordonnée X
-                y = line[38:46].strip()
-                z = line[46:55].strip()
-                print(base, position, chaine,name, x, y, z)
-            else:    
-                line = file.readline()   # On avance d'une ligne
-                continue                 # On enleve tout le reste (C, P, H, etc.)
-        
-        line = file.readline()
+                    x = line[30:38].strip()     # Coordonnée X
+                    y = line[38:46].strip()
+                    z = line[46:55].strip()
+
+                    atome_infos = {
+                        'base': base,
+                        'position': position,
+                        'chaine': chaine,
+                        'name': name,       # On rajoute ça pour l'étape 3 d'après !
+                        'coord': (x, y, z)  # On garde les coordonnées au cas où
+                    }
+                    resultat.append(atome_infos) # Correction : on utilise 'resultat'
+
+                else:    
+                    line = file.readline()   # On avance d'une ligne
+                    continue                 # On enleve tout le reste (C, P, H, etc.)
+            
+            line = file.readline()
+
+    return resultat    
+
+
+
+
+# 1.Visualisation
+mes_atomes = extraire_donnees(pdb_name) # On appelle la fonction et on stocke ce qu'elle renvoie dans 'mes_atomes'
+for atome in mes_atomes[:5]: # On affiche seulement les 5 premiers 
+    print(atome)
+
+
+
+# 2.Reconstruction de la sequence
+sequence = {}
+for atome in mes_atomes: # on parcourt tous les atomes
+
+    position = atome['position']
+    base = atome['base']
+    sequence[position] = base # On ne garde qu'une seule base par position
+
+chaine_finale = ''.join(sequence[i] for i in sorted(sequence)) # reconstruction dans l'ordre des positions
+
+print("\nSequence reconstruite :")
+print(chaine_finale)
